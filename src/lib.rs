e1f5960f67ca0f10f7438b6cd55510b2a1ca5473
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2022-05-10 20:05:37 +0200

    Add insert and part of resize, need test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
-use std::hash::Hash;
+use std::borrow::Borrow;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::mem;
 
-#[derive(Debug)]
+const INITIAL_NBUCKETS: usize = 1;
+
+#[derive(Debug, Clone)]
 pub enum Entry<K, V> {
     Empty,
     Deleted,
-    Pair(K, V),
+    Pair { key: K, value: V },
 }
 pub struct HashMap<K, V> {
     addresses: Vec<Entry<K, V>>,
@@ -24,8 +29,65 @@ impl<K, V> HashMap<K, V>
 where
     K: Hash + Eq,
 {
+    fn address<Q>(&self, key: &Q, attempt: i64) -> Option<usize>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        if self.addresses.is_empty() {
+            return None;
+        }
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        attempt.hash(&mut hasher);
+        Some((hasher.finish() % self.addresses.len() as u64) as usize)
+    }
+    fn resize(&mut self) {
+        let new_size = match self.addresses.len() {
+            0 => INITIAL_NBUCKETS,
+            n => 2 * n,
+        };
+        let mut new_addresses: Vec<Entry<K, V>> = Vec::with_capacity(new_size);
+        new_addresses.extend((0..new_size).map(|_| Entry::Empty));
+        // TODO: rehash entries from old vector to new
+        _ = mem::replace(&mut self.addresses, new_addresses);
+    }
+
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        todo!()
+        if self.addresses.is_empty() || self.items > 3 * self.addresses.len() / 4 {
+            self.resize();
+        }
+        // probe underlying vector until either same key is found
+        // or vector !Entry::Pair found
+        // None if new key was inserted, otherwise old (replaced) value
+        let mut attempt = 0;
+        let mut address = self
+            .address(&key, attempt)
+            .expect(".is_empty() handled above");
+        loop {
+            match self.addresses[address] {
+                // Empty = no entry with such key
+                Entry::Empty => {
+                    self.addresses[address] = Entry::Pair { key, value };
+                    return None;
+                }
+                // If Pair found, check key, and if key is the same, update value
+                Entry::Pair {
+                    key: ref ekey,
+                    value: ref mut evalue,
+                } => {
+                    if ekey == &key {
+                        return Some(mem::replace(evalue, value));
+                    }
+                }
+                // Deleted = continue searching
+                Entry::Deleted => (),
+            }
+            attempt += 1;
+            address = self
+                .address(&key, attempt)
+                .expect(".is_empty() handled above");
+        }
     }
 }
 
