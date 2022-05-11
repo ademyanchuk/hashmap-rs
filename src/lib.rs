0de2c6bb955555e9e12cd6dd9e24fa644f26b5ae
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2022-05-11 17:38:12 +0200

    Refactor to use struct entry and option

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@ use std::mem;
 
 const INITIAL_NBUCKETS: usize = 1;
 
-#[derive(Debug, Clone)]
-pub enum Entry<K, V> {
-    Empty,
-    Deleted,
-    Pair { key: K, value: V },
+#[derive(Debug)]
+pub struct Entry<K, V> {
+    key: K,
+    value: V,
+    deleted: bool,
 }
 pub struct HashMap<K, V> {
-    addresses: Vec<Entry<K, V>>,
+    addresses: Vec<Option<Entry<K, V>>>,
     items: usize,
 }
 
@@ -22,6 +22,12 @@ impl<K, V> HashMap<K, V> {
             addresses: Vec::new(),
             items: 0,
         }
+    }
+}
+
+impl<K, V> Default for HashMap<K, V> {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -47,10 +53,54 @@ where
             0 => INITIAL_NBUCKETS,
             n => 2 * n,
         };
-        let mut new_addresses: Vec<Entry<K, V>> = Vec::with_capacity(new_size);
-        new_addresses.extend((0..new_size).map(|_| Entry::Empty));
-        // TODO: rehash entries from old vector to new
-        _ = mem::replace(&mut self.addresses, new_addresses);
+        let mut new_addresses: Vec<Option<Entry<K, V>>> = Vec::with_capacity(new_size);
+        new_addresses.extend((0..new_size).map(|_| None));
+        for elem in self.addresses.drain(..) {
+            match elem {
+                None => continue,
+                Some(entry) => {
+                    if entry.deleted {
+                        continue;
+                    }
+                    // we need to find correct address for entry in new vector
+                    let mut attempt = 0;
+                    loop {
+                        let mut hasher = DefaultHasher::new();
+                        entry.key.hash(&mut hasher);
+                        attempt.hash(&mut hasher);
+                        let address = (hasher.finish() % new_addresses.len() as u64) as usize;
+                        match new_addresses[address] {
+                            Some(_) => {
+                                attempt += 1;
+                                continue;
+                            }
+                            None => {
+                                new_addresses[address] = Some(entry);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        self.addresses = new_addresses;
+    }
+    fn find(&self, key: &K) -> Option<usize> {
+        let mut attempt = 0;
+        loop {
+            match self.address(key, attempt) {
+                None => return None,
+                Some(address) => match self.addresses[address] {
+                    None => return None,
+                    Some(ref entry) => {
+                        if entry.key == *key {
+                            return Some(address);
+                        }
+                        attempt += 1;
+                    }
+                },
+            }
+        }
     }
     pub fn len(&self) -> usize {
         self.items
@@ -63,37 +113,38 @@ where
         if self.addresses.is_empty() || self.items > 3 * self.addresses.len() / 4 {
             self.resize();
         }
-        // probe underlying vector until either same key is found
-        // or vector !Entry::Pair found
-        // None if new key was inserted, otherwise old (replaced) value
-        let mut attempt = 0;
-        let mut address = self
-            .address(&key, attempt)
-            .expect(".is_empty() handled above");
-        loop {
-            match self.addresses[address] {
-                // Empty = no entry with such key
-                Entry::Empty => {
-                    self.addresses[address] = Entry::Pair { key, value };
-                    self.items += 1;
-                    return None;
-                }
-                // If Pair found, check key, and if key is the same, update value
-                Entry::Pair {
-                    key: ref ekey,
-                    value: ref mut evalue,
-                } => {
-                    if ekey == &key {
-                        return Some(mem::replace(evalue, value));
+        match self.find(&key) {
+            None => {
+                // insert new, update size and return None
+                let mut attempt = 0;
+                loop {
+                    let address = self
+                        .address(&key, attempt)
+                        .expect("is_empty, handled above");
+                    match self.addresses[address] {
+                        Some(_) => {
+                            attempt += 1;
+                            continue;
+                        }
+                        None => {
+                            self.addresses[address] = Some(Entry {
+                                key,
+                                value,
+                                deleted: false,
+                            });
+                            self.items += 1;
+                            return None;
+                        }
                     }
                 }
-                // Deleted = continue searching
-                Entry::Deleted => (),
             }
-            attempt += 1;
-            address = self
-                .address(&key, attempt)
-                .expect(".is_empty() handled above");
+            Some(address) => {
+                // key exists, update, return old value
+                match self.addresses[address] {
+                    None => None, // it will not be None here
+                    Some(ref mut entry) => Some(mem::replace(&mut entry.value, value)),
+                }
+            }
         }
     }
 }
@@ -106,8 +157,12 @@ mod tests {
         let mut map = HashMap::new();
         assert_eq!(map.len(), 0);
         assert!(map.is_empty());
-        map.insert("key: K", 43);
-        assert_eq!(map.len(), 1);
+        map.insert("bar", 43);
+        map.insert("foo", 42);
+        assert_eq!(map.len(), 2);
         assert!(!map.is_empty());
+        assert_ne!(map.find(&"bar"), None);
+        assert_ne!(map.find(&"foo"), None);
+        assert_eq!(map.addresses.len(), 2)
     }
 }
