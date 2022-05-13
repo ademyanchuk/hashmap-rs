202f4e076c2a9c90ff0feea5031a958ad74d8717
Alexey Demyanchuk <demyanchuk.alexey@mh-hannover.de>
2022-05-13 14:31:58 +0200

    Add remove and decontamination

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub enum Entry<K, V> {
 pub struct HashMap<K, V> {
     table: Vec<Entry<K, V>>,
     items: usize,
+    tombs: usize,
 }
 
 impl<K, V> HashMap<K, V> {
@@ -22,6 +23,7 @@ impl<K, V> HashMap<K, V> {
         HashMap {
             table: Vec::new(),
             items: 0,
+            tombs: 0,
         }
     }
 }
@@ -46,11 +48,14 @@ where
         key.hash(&mut hasher);
         hasher.finish()
     }
-    fn resize(&mut self) {
-        let new_size = match self.table.len() {
+    fn resize(&mut self, rehash_only: bool) {
+        let mut new_size = match self.table.len() {
             0 => INITIAL_SIZE,
             n => 2 * n,
         };
+        if rehash_only {
+            new_size = self.table.len();
+        }
         let mut new_table: Vec<Entry<K, V>> = Vec::with_capacity(new_size);
         new_table.extend((0..new_size).map(|_| Entry::Empty));
         for entry in self.table.drain(..) {
@@ -71,6 +76,7 @@ where
             }
         }
         self.table = new_table;
+        self.tombs = 0;
     }
     pub fn len(&self) -> usize {
         self.items
@@ -80,12 +86,13 @@ where
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        if self.table.is_empty() || self.items > 3 * self.table.len() / 4 {
-            self.resize();
+        if self.table.is_empty() || self.items >= 3 * self.table.len() / 4 {
+            self.resize(false);
         }
         let mut nv = value;
         let hash = self.prehash(&key);
         let mut idx = (hash % self.table.len() as u64) as usize;
+        let mut cnt = 0;
         while !matches!(self.table[idx], Entry::Empty) {
             // pair
             if let Entry::Pair {
@@ -100,26 +107,38 @@ where
                 }
             }
             idx = (idx + 1) % self.table.len();
+            if cnt > self.table.len() {
+                panic!("Infinite loop!")
+            }
+            cnt += 1;
         }
         // new key
         self.table[idx] = Entry::Pair { key, val: nv };
         self.items += 1;
         None
     }
-    pub fn contains_key<Q: ?Sized>(&self, key: &Q) -> bool
+    pub fn contains_key<Q: ?Sized>(&mut self, key: &Q) -> bool
     where
         K: Borrow<Q>,
         Q: Hash + Eq + Debug,
     {
         self.get(key).is_some()
     }
-    pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<&V>
+    pub fn get<Q: ?Sized>(&mut self, key: &Q) -> Option<&V>
     where
         K: Borrow<Q>,
         Q: Hash + Eq + Debug,
     {
+        if self.table.is_empty() {
+            return None;
+        }
+        // rehash if contaminated
+        if self.items + self.tombs > 3 * self.table.len() / 4 {
+            self.resize(true);
+        }
         let hash = self.prehash(key);
         let mut idx = (hash % self.table.len() as u64) as usize;
+        let mut cnt = 0;
         while !matches!(self.table[idx], Entry::Empty) {
             if let Entry::Pair { key: ek, val: ev } = &self.table[idx] {
                 if ek.borrow() == key {
@@ -129,6 +148,10 @@ where
             }
             // linear probing
             idx = (idx + 1) % self.table.len();
+            if cnt > self.table.len() {
+                panic!("Infinite loop!")
+            }
+            cnt += 1;
         }
         // not found
         None
@@ -138,7 +161,40 @@ where
         K: Borrow<Q>,
         Q: Hash + Eq + Debug,
     {
-        todo!()
+        if self.table.is_empty() {
+            return None;
+        }
+        // rehash if contaminated
+        if self.items + self.tombs > 3 * self.table.len() / 4 {
+            self.resize(true);
+        }
+        let hash = self.prehash(key);
+        let mut idx = (hash % self.table.len() as u64) as usize;
+        let mut cnt = 0;
+        while !matches!(self.table[idx], Entry::Empty) {
+            if let Entry::Pair {
+                key: ref ek,
+                val: ev,
+            } = &mut self.table[idx]
+            {
+                if ek.borrow() == key {
+                    // found and remove
+                    let mut tmp: V = Default::default();
+                    mem::swap(&mut tmp, ev);
+                    self.table[idx] = Entry::Del;
+                    self.items -= 1;
+                    self.tombs += 1;
+                    return Some(tmp);
+                }
+            }
+            // linear probing
+            idx = (idx + 1) % self.table.len();
+            if cnt > self.table.len() {
+                panic!("Infinite loop!")
+            }
+            cnt += 1;
+        }
+        None
     }
 }
 
@@ -149,9 +205,9 @@ mod tests {
     fn resize() {
         let mut map = HashMap::<&str, &str>::new();
         assert!(map.table.is_empty());
-        map.resize();
-        map.resize();
-        map.resize();
+        map.resize(false);
+        map.resize(false);
+        map.resize(false);
         assert_eq!(map.table.len(), INITIAL_SIZE * 4)
     }
     #[test]
@@ -166,16 +222,30 @@ mod tests {
         assert_eq!(map.len(), 2);
         map.insert("bazz", 123);
         assert!(map.contains_key("foo"));
-        println!("[test]: {:?}", map.table);
         assert_eq!(map.get("bar"), Some(&43));
         assert_eq!(map.get("barBazz"), None);
         assert!(!map.is_empty());
         assert!(map.contains_key("bazz"));
         assert!(map.contains_key("foo"));
         assert_eq!(map.get("foo"), Some(&42));
-        // assert_eq!(map.remove("foo"), Some(42));
-        // assert_eq!(map.get("foo"), None);
+        assert_eq!(map.remove("foo"), Some(42));
+        assert_eq!(map.get("foo"), None);
+        map.insert("foo", 42);
+        println!("[test]: {:?}", map.table);
         assert_eq!(map.table.len(), 4)
+    }
+    #[test]
+    fn contaminate() {
+        let mut map = HashMap::new();
+        map.insert(12, 21); // size 1, items 1
+        map.insert(11, 11); // size 2, items 2
+        map.insert(99, 99); // size 4, items 3
+        map.remove(&12); // size 4, items 2, 1 tomb
+        map.insert(10, 10); // size 4, items 3, 1 tomb
+                            // size is still ok and not a time to growth
+                            // but table is contaminated and will panic
+                            // on insert, get(N/E), contains_key(N/E), remove(N/E)
+        assert_eq!(map.get(&12), None);
     }
     #[test]
     fn empty_hashmap() {
